e21d0800dda2f4ee846c836a7cda5f6d89f9b0d8
En4k8 <93187124+En4k8@users.noreply.github.com>
2022-05-16 13:01:49 +0300

    Add files via upload

---
+++ Задание от 04.05.2022/WindowMessageBox/ErrorProvider/ErrorProvider/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace ErrorProvider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.numberBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(53, 96);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(193, 22);
+            this.nameBox.TabIndex = 0;
+            // 
+            // numberBox
+            // 
+            this.numberBox.Location = new System.Drawing.Point(53, 199);
+            this.numberBox.Name = "numberBox";
+            this.numberBox.Size = new System.Drawing.Size(193, 22);
+            this.numberBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите имя:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите номер телефона:";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 291);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numberBox);
+            this.Controls.Add(this.nameBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox numberBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Задание от 04.05.2022/WindowMessageBox/ErrorProvider/ErrorProvider/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ErrorProvider
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            nameBox.Validating += nameBox_Validating;
+            numberBox.Validating += numberBox_Validating;
+        }
+
+        private void nameBox_Validating(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(nameBox.Text))
+            {
+                errorProvider1.SetError(nameBox, "Не указано имя!");
+            }
+            else if (nameBox.Text.Length < 4)
+            {
+                errorProvider1.SetError(nameBox, "Слишком короткое имя!");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+        private void numberBox_Validating(object sender, EventArgs e)
+        {
+            int number = 0;
+            if (String.IsNullOrEmpty(numberBox.Text))
+            {
+                errorProvider1.SetError(numberBox, "Не указан телефон!");
+            }
+            else if (!Int32.TryParse(numberBox.Text, out number))
+            {
+                errorProvider1.SetError(numberBox, "Некорретный телефон!");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+    }
+}
